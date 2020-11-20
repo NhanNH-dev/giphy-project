3e7f371f80429300415a8e4880bb70b0f73b7454
unknown <nguyenhuunhan4692@gmail.com>
2020-11-20 16:27:36 +0700

    len nao

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.APP_DEVELOPMENT;
+export const API = "https://api.giphy.com/v1/gifs/search";
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 export const API_KEY = publicRuntimeConfig.API_KEY;
