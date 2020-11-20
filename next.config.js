23c844d39cef8beba0c7d5b434bb9acdf4a59e53
unknown <nguyenhuunhan4692@gmail.com>
2020-11-20 16:09:05 +0700

    len nao

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 import withCSS from "@zeit/next-css";
-module.exports = withCSS({
+export default withCSS({
   publicRuntimeConfig: {
     APP_NAME: "GIPHY",
     APP_DEVELOPMENT: "https://api.giphy.com/v1/gifs/search",
