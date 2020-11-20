c39e80ba7b1b86166cc8ad4fae4592262bb7b382
unknown <nguyenhuunhan4692@gmail.com>
2020-11-20 17:03:48 +0700

    len nao

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/style.css";
 import "../node_modules/nprogress/nprogress.css";
 import dynamic from "next/dynamic";
-
+import Head from "next/head";
 const TopProgressBar = dynamic(
   () => {
     return import("../components/TopProgressBar");
@@ -13,9 +13,11 @@ const TopProgressBar = dynamic(
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script type="module"></script>
+      </Head>
       <TopProgressBar />
       <Component {...pageProps} />
-      <script type="module"></script>
     </>
   );
 }
