ac4bbc56d58e186136455e9c70c58ad7aed4e467
unknown <nguyenhuunhan4692@gmail.com>
2020-11-25 13:31:55 +0700

    giphy-project || fix next.config.js

--- next.config.cjs
+++
@@ -1,9 +0,0 @@
-import withCSS from "@zeit/next-css";
-module.exports = withCSS({
-  publicRuntimeConfig: {
-    APP_NAME: "GIPHY",
-    APP_DEVELOPMENT: "https://api.giphy.com/v1/gifs/search",
-    PRODUCTION: false,
-    API_KEY: "4ehJ726BPs10cGcXK4FrIlJAsjqPqywX",
-  },
-});
---
+++ next.config.js
@@ -0,0 +1,10 @@
+import withCSS from "@zeit/next-css";
+export default withCSS()
+// module.exports = withCSS({
+//   publicRuntimeConfig: {
+//     APP_NAME: "GIPHY",
+//     APP_DEVELOPMENT: "https://api.giphy.com/v1/gifs/search",
+//     PRODUCTION: false,
+//     API_KEY: "4ehJ726BPs10cGcXK4FrIlJAsjqPqywX",
+//   },
+// });
