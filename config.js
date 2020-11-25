08e30e1f1f421f4c993e3b20863884d5d2b8ccc2
unknown <nguyenhuunhan4692@gmail.com>
2020-11-25 17:39:55 +0700

    fix error

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
-export const API = "https://api.giphy.com/v1/gifs/search";
-export const APP_NAME = "GIPHY";
-export const API_KEY = "4ehJ726BPs10cGcXK4FrIlJAsjqPqywX";;
+export const API = publicRuntimeConfig.APP_DEVELOPMENT;
+export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const API_KEY = publicRuntimeConfig.API_KEY;
