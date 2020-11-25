39d624a4b462e7492cdd9174d38ee806819534cd
unknown <nguyenhuunhan4692@gmail.com>
2020-11-25 17:31:37 +0700

    new branch

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const { publicRuntimeConfig } = getConfig();
 
 export const API = "https://api.giphy.com/v1/gifs/search";
 export const APP_NAME = "GIPHY";
-export const API_KEY = "4ehJ726BPs10cGcXK4FrIlJAsjqPqywX";
+export const API_KEY = "4ehJ726BPs10cGcXK4FrIlJAsjqPqywX";;
