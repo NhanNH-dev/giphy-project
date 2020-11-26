d9c70924eaa7b1d5324d2567ae700490c1180862
Nhan Nguyen <55623692+NhanNH-dev@users.noreply.github.com>
2020-11-26 08:12:35 +0700

    giphy-project || rm config

--- config.js
+++
@@ -1,6 +0,0 @@
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
-
-export const API = "https://api.giphy.com/v1/gifs/search";
-export const APP_NAME = "GIPHY";
-export const API_KEY = "4ehJ726BPs10cGcXK4FrIlJAsjqPqywX";
