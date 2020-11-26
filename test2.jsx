097d658ebd805a0925b87c3e91e8e2867930973b
DESKTOP-ENJFCBP\dell <nguyenhuunhan4692@gmail.com>
2020-11-26 13:51:18 +0700

    dev || them the span

---
+++ test2.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class test2 extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                class component!
+                <span> them the span</span>
+            </React.Fragment>
+        )
+    }
+}
+
+export default test2
