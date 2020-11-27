865bc7d69f0c2d25c81fa5aab11ecbe22f50811e
DESKTOP-ENJFCBP\dell <nguyenhuunhan4692@gmail.com>
2020-11-27 08:34:40 +0700

    master || change UI & touch in mobile

--- components/GirdImage.js
+++ components/GirdImage.js
@@ -5,7 +5,9 @@ const GirdImage = ({ listGif, handleClick }) => (
   <div className="row row-cols-1 row-cols-md-4">
     {listGif &&
       listGif.map((img, i) => (
-        <ItemImage key={i} handleClick={handleClick} image={img} />
+        <div key={img.id} className="card-columns">
+          <ItemImage  handleClick={handleClick} image={img} />
+        </div>
       ))}
   </div>
 );
